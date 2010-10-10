a953ada3d8501684d16a70627d186cad90736cc2
rafBM <rafbmasson@gmail.com>
2010-10-10 02:46:18 -0400

    Renamed "titleElement" to "headingElement" all over the place.

--- src/heading-anchors.js
+++ src/heading-anchors.js
@@ -17,23 +17,23 @@ window.HeadingAnchors = {
         
         var niceHeadings = {};
         
-        $('h2, h3, h4, h5, h6').forEach(function( titleElement ) {
+        $('h2, h3, h4, h5, h6').forEach(function( headingElement ) {
             
             var anchor    = document.createElement('a'),
-                niceHeading = nice( titleElement.innerHTML );
+                niceHeading = nice( headingElement.innerHTML );
             
             if ( niceHeading in niceHeadings ) {
                 anchor.href = '#-'+ niceHeading +'-'+ niceHeadings[niceHeading];
-                titleElement.id = '-'+ niceHeading +'-'+ niceHeadings[niceHeading];
+                headingElement.id = '-'+ niceHeading +'-'+ niceHeadings[niceHeading];
                 niceHeadings[niceHeading] = niceHeadings[niceHeading] += 1;
             } else {
                 anchor.href = '#-'+ niceHeading;
-                titleElement.id = '-'+ niceHeading;
+                headingElement.id = '-'+ niceHeading;
                 niceHeadings[niceHeading] = 2;
             }
             
-            titleElement.className += ' heading-anchor';
-            titleElement.appendChild( anchor );
+            headingElement.className += ' heading-anchor';
+            headingElement.appendChild( anchor );
         });
         
         var headingInHash = document.getElementById( window.location.hash.substr(1) )
