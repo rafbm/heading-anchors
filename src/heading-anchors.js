cc271dcade38373cb84956d647fc32bdf4ca6da3
Rafaël Blais Masson <rafbmasson@gmail.com>
2012-02-08 23:47:14 -0500

    Formatting and stuff

--- src/heading-anchors.js
+++ src/heading-anchors.js
@@ -1,7 +1,7 @@
 /*
- *  Heading Anchors v1.0.1
- *  Copyright (c) 2010-2011 Rafaël Blais Masson <http://twitter.com/rafBM>
- *  
+ *  Heading Anchors v1.0.2
+ *  Copyright (c) 2010-2012 Rafaël Blais Masson <http://twitter.com/rafBM>
+ *
  *  Freely distributable under the terms of the MIT license.
  *  <http://github.com/rafBM/heading-anchors>
  *
@@ -9,53 +9,44 @@
 
 window.HeadingAnchors = {
 
-  init: function(customHeadingsSelector) {
-    
-    if (document.querySelectorAll) {
-      var $ = function(selector) {
-        var elements = document.querySelectorAll(selector)
-          , elementsArray = []
-        
-        for (var l = elements.length, e = 0; e < l; e++)
-          elementsArray.push(elements[e])
-        
-        return elementsArray
-      }
-      
-      var nice = function(notNiceString) {
-        return notNiceString.replace(/['’]/g,'').replace(/[^a-z0-9]+/ig, '-')
-      }
-      
-      var niceHeadings = {}, headingsSelector
-      
-      if (typeof customHeadingsSelector == "string" && customHeadingsSelector.length)
-        headingsSelector = customHeadingsSelector
-      else
-        headingsSelector = 'h2, h3, h4, h5, h6'
-      
-      $(headingsSelector).forEach(function(headingElement) {
-        var anchor = document.createElement('a')
-          , niceHeading = nice(headingElement.id ? headingElement.id : headingElement.innerHTML.replace(/<([^ ]+)[^<>]*>([^<>]*)<\/\1>/ig, '$2'))
-        
-        anchor.className = 'heading-anchor'
-        
-        if (niceHeadings[ niceHeading ]) {
-          anchor.href = '#' + niceHeading + '-' + niceHeadings[ niceHeading ]
-          headingElement.id = niceHeading + '-' + niceHeadings[ niceHeading ]
-          niceHeadings[ niceHeading ] = niceHeadings[ niceHeading ] += 1
-        } else {
-          anchor.href = '#' + niceHeading
-          headingElement.id = niceHeading
-          niceHeadings[ niceHeading ] = 2
-        }
-        
-        anchor.innerHTML = '¶'
-        headingElement.appendChild(anchor)
-      })
-      
-      var headingInHash = document.getElementById(window.location.hash.substr(1))
-      if (headingInHash)
-        window.scrollTo(0, headingInHash.offsetTop)
+  init: function(customSelector) {
+    if (!document.querySelectorAll || ![].forEach)
+      return
+
+    var $ = function(selector) {
+      return [].slice.call(document.querySelectorAll(selector), 0)
     }
+
+    var slugize = function(str) {
+      return str.replace(/['’]/g, '').replace(/[^a-z0-9]+/ig, '-')
+    }
+
+    var existingSlugsNumbers = {}
+      , selector = customSelector ? customSelector : 'h2, h3, h4'
+
+    $(selector).forEach(function(heading) {
+      var anchor = document.createElement('a')
+        , slug = slugize(heading.id ? heading.id : heading.textContent)
+
+      anchor.className = 'heading-anchor'
+
+      // if slug #Foo-bar already exists, create #Foo-bar-2, #Foo-bar-3 and so on
+      if (slug in existingSlugsNumbers) {
+        existingSlugsNumbers[slug] += 1
+        slug += '-' + existingSlugsNumbers[slug]
+      } else {
+        existingSlugsNumbers[slug] = 1
+      }
+
+      anchor.href = '#' + slug
+      heading.id = slug
+
+      anchor.innerHTML = '¶'
+      heading.appendChild(anchor)
+    })
+
+    var headingInHash = document.getElementById(location.hash.substr(1))
+    if (headingInHash)
+      window.scrollTo(0, headingInHash.offsetTop)
   }
 }
